99ba54150d0f0f756ad47fcb9068af724cd0cb4a
Ak1ne <37769458+KNO110@users.noreply.github.com>
2024-06-24 17:30:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
-function Fraction(numm, denom) {
-    this.numm= numm
-    this.denom = denom;
-}
-Fraction.prototype.divide = function(fraction) {
-    return new Fraction(
-        this.numm * fraction.denom,
-        this.denom * fraction.numm
-    );
-};
-Fraction.prototype.gcd = function(a, b) {   ///gcd самый большой общий делитель
-    if (!b) {
+class Fraction {
+    constructor(numm, denom) {
+        this.numm = numm;
+        this.denom = denom;
+    }
+
+    divide(fraction) {
+        return new Fraction(
+            this.numm * fraction.denom,
+            this.denom * fraction.numm
+        );
+    }
+
+    gcd(a, b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        while (b) {
+            let temp = b;
+            b = a % b;
+            a = temp;
+        }
         return a;
     }
-    return this.gcd(b, a % b);
-};
 
-Fraction.prototype.simplify = function() {
-    const gcd = this.gcd(this.numm, this.denom);
-    return new Fraction(this.numm / gcd, this.denom / gcd);
-};
+    simplify() {
+        const common = this.gcd(this.numm, this.denom);      ///gcd самый большой общий делитель
+        return new Fraction(this.numm / common, this.denom / common);
+    }
+
+    toString() {
+        return `${this.numm}/${this.denom}`;
+    }
+}
+
 const fraction_1 = new Fraction(3, 4);
 const fraction_2 = new Fraction(5, 6);
 
 const result = fraction_1.divide(fraction_2);
-console.log(`Result: ${result.numm}/${result.denom}`);
+console.log(`Result: ${result}`);
 
 const simp_result = result.simplify();
-console.log(`Simplified Result: ${simp_result.numm}/${simp_result.denom}`);
+console.log(`Simplified Result: ${simp_result}`);
